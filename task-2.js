49fa0234298fd2fc3b7ac7edf598770a171e0771
Mariana987 <m1a1r1y1a1n1a1@gmail.com>
2021-08-17 11:39:04 +0200

    .

--- task-2.js
+++ task-2.js
@@ -1,4 +1,14 @@
-Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback - функцию callback,
+//   а принимала всего два параметра allUsers и userName и возвращала промис.
+
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+// };
+
 
 const users = [
   { name: 'Mango', active: true },
@@ -7,24 +17,19 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
 
-  callback(updatedUsers);
+    resolve(updatedUsers);
+  });
 };
 
-const logger = updatedUsers => console.table(updatedUsers);
+const logger_2 = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
+toggleUserState(users, 'Mango').then(logger_2);
+toggleUserState(users, 'Lux').then(logger_2);
 
-/*
- * Должно работать так
- */
-toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Lux').then(logger);
+
--- task-3.js
+++ task-3.js
@@ -1,24 +1,27 @@
-Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
+// Перепиши функцию makeTransaction() так, чтобы она не использовала callback - функции onSuccess и onError,
+//   а принимала всего один параметр transaction и возвращала промис.
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
-  const delay = randomIntegerFromInterval(200, 500);
+const makeTransaction = ({ id, amount }) => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
 
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
+      if (canProcess) {
+        resolve([id, delay]);
+      } else {
+        reject(id);
+      }
+    }, delay);
+  });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -26,16 +29,6 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
@@ -51,3 +44,4 @@ makeTransaction({ id: 72, amount: 75 })
 makeTransaction({ id: 73, amount: 100 })
   .then(logSuccess)
   .catch(logError);
+
